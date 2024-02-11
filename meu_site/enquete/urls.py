73aa1ed01e26330681dede82465b75ced01ad2ff
Renatoeletronico <rfs_eletronico@hotmail.com>
2024-02-11 14:25:12 -0300

    teste 3 git ignore

--- meu_site/enquete/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include 
-from . import views 
-
-app_name = 'enquete'
-urlpatterns = [
-    path('',views.index, name='index'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-]
